1d6b73116d35741fb983af56ad408cdc61ba4b29
Roland Sharp <rolandnsharp@gmail.com>
2014-06-15 16:17:42 +1000

    example working

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,11 @@
-var resume - to - text = require('../resumeToText');
+var fs = require('fs');
+var resumeToText = require('..');
 
 var resumeInput = process.argv[2]; //value will be "banana"  for testing
+var resumeOutput = process.argv[3];
 
 var resumeData = JSON.parse(fs.readFileSync(resumeInput, 'utf8'));
-for testing
 
 resumeToText(resumeData, function(TextResume) {
-    console.log(TextResume);
+    fs.writeFileSync(resumeOutput, TextResume, 'utf8');
 });
