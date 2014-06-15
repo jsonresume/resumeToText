071c2095dc654ebb3fd726175f1d248b63b0c14e
Roland Sharp <rolandnsharp@gmail.com>
2014-06-15 15:59:18 +1000

    6'

--- resumeToText.js
+++ resumeToText.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 var Mustache = require('mustache');
+var path = require('path');
 
-var resumeTemplate = fs.readFileSync(__dirname + '/layout.template', 'utf8');
+var resumeTemplate = fs.readFileSync(path.resolve(__dirname, 'layout.template'), 'utf8');
 
 function resumeToText(resumeObject, callback) {
     var resumeTXT = Mustache.render(resumeTemplate, resumeObject);
